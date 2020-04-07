339b8714b1a1fd07f4a7cfa0b3be6b9e82975ace
joaopaulofranca <joaopaulofrancasilva@outlook.com>
2020-04-07 17:37:26 -0300

    Projeto DBZmail

---
+++ app/index.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+    	<title>E-mail</title>
+		<link rel="stylesheet" href="../css/styles.css">
+
+	</head>
+<body>
+	<div class="principal">
+		<div class="container">
+		<form class="formulario" action="" method="post">
+			<div class="form-group">
+				<label for="para">Para</label><br>
+				<input name="para" type="text" class="form-control" id="para" placeholder="joao@dominio.com.br">
+			</div><br>
+
+			<div class="form-group">
+				<label for="assunto">Assunto</label><br>
+				<input name="assunto" type="text" class="form-control" id="assunto" placeholder="Assundo do e-mail">
+			</div><br>
+			<div class="form-group">
+				<label for="mensagem">Mensagem</label><br>
+				<textarea name="mensagem" class="form-control" id="mensagem"></textarea>
+			</div><br>
+
+			<button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+		</form>
+		</div>
+		<div class="container2">
+			<img src="../img/esferas.png" alt="" class="rotate">
+		</div>
+	</div>
+</body>
+</html>
